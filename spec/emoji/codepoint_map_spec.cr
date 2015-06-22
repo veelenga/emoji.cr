4db9393fb86cc87e0b96a931103149fb6957b3f8
veelenga <velenhaupt@gmail.com>
2015-06-22 11:40:35 +0300

    More precise spec description

--- spec/emoji/codepoint_map_spec.cr
+++ spec/emoji/codepoint_map_spec.cr
@@ -6,21 +6,21 @@ module Emoji
     codepoint = "\u{1f431}"
 
     describe "#includes?" do
-      it "returns true for existed codename" do
+      it "returns true for existed codepoint name" do
         CodepointMap.new.includes?(name).should be_true
       end
 
-      it "returns false for missed codename" do
+      it "returns false for missed codepoint name" do
         CodepointMap.new.includes?(":no_such_codepoint_name:").should be_false
       end
     end
 
     describe "#[]" do
-      it "returns codepoint if such name exists" do
+      it "returns codepoint by codepoint name" do
         CodepointMap.new[name].should eq(codepoint)
       end
 
-      it "raises exeception otherwise" do
+      it "raises MissingKey if such codepoint name missed" do
         expect_raises MissingKey do
           CodepointMap.new[":no_such_codepoint_name:"]
         end
