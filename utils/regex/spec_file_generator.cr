5bb52e87fd5a4bb4c177f87fbeb15cbef5fe0a99
Anton Maminov <anton.linux@gmail.com>
2019-02-10 22:44:50 +0200

    re-generate regex spec

--- utils/regex/spec_file_generator.cr
+++ utils/regex/spec_file_generator.cr
@@ -5,13 +5,8 @@ macro generate_source(data)
 require "../spec_helper"
 {% for name in data %}
 it "`{{name[0].id}}` ({{name[3].id}}) should match `{{name[2].id}}` ({{name[1].id}})" do
-  if m = "{{name[1].id}}".match(Emoji::EMOJI_REGEX)
-    "{{name[1].id}}".match(Emoji::EMOJI_REGEX).try(&.[0]).should eq "{{name[1].id}}"
-  else
-    fail("`{{name[0].id}}` doesn't match `{{name[2].id}}` ({{name[1].id}})")
-  end
-end
-{% end %}
+  "{{name[1].id}}".match(Emoji::EMOJI_REGEX).try(&.[0]).should eq "{{name[1].id}}"
+end{% end %}
 EOT
 end
 
